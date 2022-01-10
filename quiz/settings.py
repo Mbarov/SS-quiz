7f292c6c5d1dbf1a1b1b77c7ceb221824c938410
Mbarov <prom@tgv73.ru>
2022-01-10 22:02:49 +0400

    create app

--- quiz/settings.py
+++ quiz/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'quiz_app'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
---
+++ quiz_app/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+from quiz.dto import *
+# Create your models here.
