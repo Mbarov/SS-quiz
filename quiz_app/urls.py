45f17f56b630d6a04b4bea30ac5ad3d4dd1bb229
Mbarov <prom@tgv73.ru>
2022-01-15 20:35:41 +0400

    общий функционал2

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .import views
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('<question>', views.questions, name='questions'),
-    #path('save_user_answer', views.save_user_answer, name='save_user_answer'),
     ]
