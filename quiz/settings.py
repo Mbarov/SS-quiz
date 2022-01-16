cf1bd90b259a78f33504e505ab7e42bba2135c04
Mbarov <prom@tgv73.ru>
2022-01-16 18:04:11 +0400

    Стилизация

--- quiz/settings.py
+++ quiz/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [ 
+    BASE_DIR / 'static',
+]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = BASE_DIR / 'db.sqlite3'
+MEDIA_URL = 'templates/images/'
